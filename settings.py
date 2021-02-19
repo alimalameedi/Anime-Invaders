a0748a51a4e2c1a528684d3bddf872b777a5b287
Ali Alameedi <alialameedi@outlook.com>
2021-02-19 12:33:38 -0800

    Fixed the fleet dropping ability and adjusted speed.

--- settings.py
+++ settings.py
@@ -14,7 +14,7 @@ class Settings:
         # self.bullet_color = [60, 60, 60]
         self.bullets_allowed = 3
         self.hinata_speed = 1.0
-        self.fleet_drop_speed = 1
+        self.fleet_drop_speed = 10
         # 1 = right; -1 = left
         self.fleet_direction = 1
 
